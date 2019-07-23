90bfc83bba4ad56b5e3af8408e998affeff8b89f
Konstantin Bogdanoski <konstantin.b@live.com>
2019-07-23 15:01:53 +0200

    Add Lab02

---
+++ Lab02/cmake-build-debug/CMakeFiles/Lab02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+        "CMakeFiles/Lab02.dir/L02.1.c.o"
+        "Lab02.pdb"
+        "Lab02"
+        )
+
+# Per-language clean rules from dependency scanning.
+foreach (lang C)
+    include(CMakeFiles/Lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach ()
